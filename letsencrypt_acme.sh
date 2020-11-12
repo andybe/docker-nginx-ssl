9589a42afd6d8510ce838770496e8bc200f432e9
andybe <andybe@gmail.com>
2020-11-12 04:24:52 +0100

    remove --net=host

--- letsencrypt_acme.sh
+++ letsencrypt_acme.sh
@@ -1,4 +1,4 @@
-docker run -it --rm --net=host -v /etc/letsencrypt:/etc/letsencrypt \
+docker run -it --rm -v /etc/letsencrypt:/etc/letsencrypt \
                     -v /var/log/letsencrypt:/var/log/letsencrypt \
                     -v /var/www:/var/www \
                     certbot/certbot \
